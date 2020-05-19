abd8f2d918dbe3ae114cf7a63d09287e392c76c7
Reyansh <reyansh2511@gmail.com>
2020-05-19 20:58:45 +0530

    Fixed Bug

--- sketch.js
+++ sketch.js
@@ -16,13 +16,13 @@ function draw() {
     deformation = 0.5*weight*speed*speed/22500;
     console.log(deformation)
     if(deformation<=100) {
-      car.shapeColor = color(0, 255, 0)
+      car.shapeColor = color(255, 0, 0)
     } 
     if(deformation>=100 || deformation<=180) {
       car.shapeColor = color(230, 230, 0)
     }
     if(deformation>=180) {
-      car.shapeColor = color(255, 0, 0)
+      car.shapeColor = color(0, 255, 0)
     } 
   }
 
